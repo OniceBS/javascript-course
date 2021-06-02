22b7243f07c397a673f978a7ef7538bb293b53a2
OniceBS <santiagoromom@hotmail.com>
2021-06-01 23:24:34 -0500

    Solución al problema de la parte 1 del curso de Dalto

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,9 +1,36 @@
 
+dineroR = prompt("Cúanto dinero tiene Roberto?");
 
-nombre = "Santiago"
+// dineroC = prompt("Cúanto dinero tiene Cofla?");
 
-frase = `<br> soy ${nombre}, mucho gusto`
+// dineroP = prompt("Cúanto dinero tiene Pedro?");
+
+if (dineroR < 0.6) {
+
+    document.write("Roberto tiene " + dineroR + " USD y no puede comprar ningún helado");
+
+}  else if (dineroR > 1.6) {
+
+    document.write("Roberto tiene " + dineroR + " USD y puede comprar cualquier helado");
+
+}   else if (dineroR < 1) {
+
+    res = dineroR % 0.6;
+
+    document.write("Roberto tiene " + dineroR + " USD y el helado más caro que puede comprar es: Palito de helado de agua y las vueltas son " + res);
+
+}   else if (dineroR < 1.6) {
+
+    res2 = dineroR % 1;
+
+    document.write("Roberto tiene " + dineroR + " USD y el helado más caro que puede comprar es: Palito de crema y las vueltas son " + res2);
+
+}   else if (dineroR = 1.6) {
+
+    res3 = dineroR % 1.6;
+
+    document.write("Roberto tiene " + dineroR + " USD y el helado más caro que puede comprar es: Palito de HELADIX y las vueltas son " + res3);
+
+}   
 
 
-
-document.write(frase); 
